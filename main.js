49c777882a481ef6945c2293b2fc1db22ded6ebf
Avi <68050609+avi-l@users.noreply.github.com>
2023-05-11 19:03:25 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// Look for the skip intro button on the page
 var skipIntroButton = ''
 const observer = new MutationObserver(function (mutations) {
   skipIntroButton = document.querySelector('.button-primary.watch-video--skip-content-button')
-  console.log({skipIntroButton})
   if (skipIntroButton !== null) {
-  skipIntroButton.click()
-  console.log('clicked!')
-  observer.disconnect();
-}
+    skipIntroButton.click()
+    observer.disconnect();
+  }
 }) 
 observer.observe(document.documentElement, {
   attributes: true,
